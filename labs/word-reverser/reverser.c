574546a2007d9c6b31f049400968b85c73f88b62
Emanuel Estrada Larios <emamex98@gmail.com>
2020-03-03 18:52:14 -0600

    Solve word-reverser lab

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    // Place your magic here
-    return 0;
+
+    while(1) {
+
+        int i = 0;
+        char input[250]; 
+
+        while((input[i] = getchar())!='\n'){
+            i++;
+            if(input[i] == EOF)
+                return 0;
+        }
+        
+        for(i=i; i>=0; i--){
+            printf("%c", input[i]);
+        }
+        
+        printf("\n");
+    }
 }
