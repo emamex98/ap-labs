2a4b20aaa39d4e98f96cee4ab40327ee5d07d2b9
Emanuel Estrada Larios <emamex98@gmail.com>
2020-03-25 23:55:17 -0600

    Solved str-lib

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include "strlib.c"
 
-int main() {
+int main(int argc, char *argv[]) {
+
+    if(argc != 4){
+        printf("Error: Missing arguments: <OriginalString> <AddString> <SubString>");
+        exit(1);
+    }
+
+    char* oStr = argv[1];
+    char* ccStr = argv[2];
+    char* sbStr = argv[3];
+    char* newStr = mystradd(oStr, ccStr);
+
+    printf("Initial lenght      : %d\n", mystrlen(oStr));
+    printf("New string          : %s\n", newStr);
+
+    if (mystrfind(newStr, sbStr)==1){
+        printf("Substring was found : yes\n");
+    } else {
+        printf("Substring was found : no\n");
+    }
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,77 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+
+    int length = 0;
+    int i;
+
+    for(i = 0; str[i] != '\0'; i++){
+        length++;
+    }
+
+    return length;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+
+    char *ccStr;
+
+    int oLen = mystrlen(origin);
+    int aLen = mystrlen(addition);
+    int tLen = oLen + aLen;
+    int i = 0, j = 0;
+
+    ccStr = malloc(sizeof(char) * (tLen + 1));
+
+    for(i = 0; i < oLen; i++){
+        ccStr[i] = origin[i];
+    }
+
+    for(j = 0; j < aLen; j++){
+        ccStr[i] = addition[j];
+        i++;
+    }
+
+    ccStr[tLen] = '\0';
+    return ccStr;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+
+    int oLen = mystrlen(origin);
+    int ssLen = mystrlen(substr);
+    int match = 0;
+    int i = 0, j = 0;
+    
+    if (ssLen == 0 || ssLen > oLen){
+        return 0;
+    }
+
+    for(i = 0; i < ((oLen + 1) - ssLen); i++){
+
+        // Check if first letter marches
+        if (origin[i] == substr[0]){
+
+            match = 1;
+
+            // Return true if substring is of size 1
+            if(ssLen == 1){
+                return 1;
+            }
+
+            // Continue looking for strinf, break if any char is different from sequence
+            for(j = 1; j < ssLen; j++){
+                if(origin[i+j] != substr[j]){
+                    match = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (match == 1){
+        return 1;
+    } else {
+        return 0;
+    }
 }
