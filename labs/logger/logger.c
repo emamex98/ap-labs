1354e507c881bbcad16c4a176de54ce537b50009
Emanuel Estrada Larios <emamex98@gmail.com>
2020-04-14 23:29:59 -0500

    Solved logger lab
    Signed-off-by: Emanuel Estrada Larios <emamex98@gmail.com>

--- labs/logger/logger.c
+++ labs/logger/logger.c
@@ -2,33 +2,182 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdarg.h>
 
 
-int printColoured(const char *format, char *colour){
+int infof(const char *format, ...){
 
 	if(strlen(format) == 0){
-		return -1;
+                return -1;
+        }
+
+	int i;
+
+	va_list arg;
+	va_start (arg, format);
+
+	write(1, "\033[32m", strlen("\033[32m"));
+
+	for(i=0; i<strlen(format); i++) {
+		if (format[i] == '%' && i+1 < strlen(format)) {
+			if (format[i+1] == 'd') {
+				int num  = va_arg(arg, int);
+				char *numStr = malloc(sizeof(char)*num);
+				sprintf(numStr, "%d", num);
+				write(1, numStr, strlen(numStr));
+				i++;
+			} else if (format[i+1] == 'f') {
+				double num  = va_arg(arg, double);
+                                char *numStr = malloc(sizeof(char)*num);
+                                sprintf(numStr, "%f", num);
+                                write(1, numStr, strlen(numStr));
+                                i++;
+			} else if (format[i+1] == 's') {
+				char *str = va_arg(arg, char*);
+				write(1, str, strlen(str));
+				i++;
+			}
+		} else {
+			write(1, &format[i], sizeof(char));
+		}
 	}
 
-	write(1, colour, strlen(colour));
-	write(1, format, strlen(format));
+
+	va_end(arg);
 	write(1, "\033[m\n", strlen("\033[m\n"));
 
-	return 0;
+  	return 0;
 }
 
-int infof(const char *format){
-        return printColoured(format, "\033[32m");
+int warnf(const char *format, ...){
+
+	if(strlen(format) == 0){
+                return -1;
+        }
+
+	int i;
+
+	va_list arg;
+	va_start (arg, format);
+
+	write(1, "\033[93m", strlen("\033[93m"));
+
+	for(i=0; i<strlen(format); i++) {
+		if (format[i] == '%' && i+1 < strlen(format)) {
+			if (format[i+1] == 'd') {
+				int num  = va_arg(arg, int);
+				char *numStr = malloc(sizeof(char)*num);
+				sprintf(numStr, "%d", num);
+				write(1, numStr, strlen(numStr));
+				i++;
+			} else if (format[i+1] == 'f') {
+				double num  = va_arg(arg, double);
+                                char *numStr = malloc(sizeof(char)*num);
+                                sprintf(numStr, "%f", num);
+                                write(1, numStr, strlen(numStr));
+                                i++;
+			} else if (format[i+1] == 's') {
+				char *str = va_arg(arg, char*);
+				write(1, str, strlen(str));
+				i++;
+			}
+		} else {
+			write(1, &format[i], sizeof(char));
+		}
+	}
+
+
+	va_end(arg);
+	write(1, "\033[m\n", strlen("\033[m\n"));
+
+  	return 0;
 }
 
-int warnf(const char *format){
-        return printColoured(format, "\033[93m");
+int errorf(const char *format, ...){
+
+	if(strlen(format) == 0){
+                return -1;
+        }
+
+	int i;
+
+	va_list arg;
+	va_start (arg, format);
+
+	write(1, "\033[31m", strlen("\033[31m"));
+
+	for(i=0; i<strlen(format); i++) {
+		if (format[i] == '%' && i+1 < strlen(format)) {
+			if (format[i+1] == 'd') {
+				int num  = va_arg(arg, int);
+				char *numStr = malloc(sizeof(char)*num);
+				sprintf(numStr, "%d", num);
+				write(1, numStr, strlen(numStr));
+				i++;
+			} else if (format[i+1] == 'f') {
+				double num  = va_arg(arg, double);
+                                char *numStr = malloc(sizeof(char)*num);
+                                sprintf(numStr, "%f", num);
+                                write(1, numStr, strlen(numStr));
+                                i++;
+			} else if (format[i+1] == 's') {
+				char *str = va_arg(arg, char*);
+				write(1, str, strlen(str));
+				i++;
+			}
+		} else {
+			write(1, &format[i], sizeof(char));
+		}
+	}
+
+
+	va_end(arg);
+	write(1, "\033[m\n", strlen("\033[m\n"));
+
+  	return 0;
 }
 
-int errorf(const char *format){
-        return printColoured(format, "\033[31m");
-}
+int panicf(const char *format, ...){
 
-int panicf(const char *format){
-        return printColoured(format, "\033[30;41m");
+	if(strlen(format) == 0){
+                return -1;
+        }
+
+	int i;
+
+	va_list arg;
+	va_start (arg, format);
+
+	write(1, "\033[30;41m", strlen("\033[30;41m"));
+
+	for(i=0; i<strlen(format); i++) {
+		if (format[i] == '%' && i+1 < strlen(format)) {
+			if (format[i+1] == 'd') {
+				int num  = va_arg(arg, int);
+				char *numStr = malloc(sizeof(char)*num);
+				sprintf(numStr, "%d", num);
+				write(1, numStr, strlen(numStr));
+				i++;
+			} else if (format[i+1] == 'f') {
+				double num  = va_arg(arg, double);
+                                char *numStr = malloc(sizeof(char)*num);
+                                sprintf(numStr, "%f", num);
+                                write(1, numStr, strlen(numStr));
+                                i++;
+			} else if (format[i+1] == 's') {
+				char *str = va_arg(arg, char*);
+				write(1, str, strlen(str));
+				i++;
+			}
+		} else {
+			write(1, &format[i], sizeof(char));
+		}
+	}
+
+
+	va_end(arg);
+	write(1, "\033[m\n", strlen("\033[m\n"));
+
+  	abort();
 }
--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,14 +1,21 @@
-int infof(char*);
-int warnf(char*);
-int errorf(char*);
-int panicf(char*);
+int infof(char*, ...);
+int warnf(char*, ...);
+int errorf(char*, ...);
+int panicf(char*, ...);
 
 int main() {
 
-	infof("Info");
-        warnf("Warning");
-        errorf("Error");
-        panicf("Panic");
+	int value1 = 404;
+	int value2 = 500;
+	float floatVal = 3.1416;
+	char *str = "EMERGENCY";
+
+	infof("Info - %f", floatVal);
+        warnf("Warning - %d", value1);
+        errorf("Error - %d %d", value1, value2);
+        panicf("Panic - %s!", str);
+
+	infof("This should not print");
 
 	return 0;
 }
