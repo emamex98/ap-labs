50b24c10aee977fa01a044643bdce9f2c03b70f3
Emanuel Estrada Larios <emamex98@gmail.com>
2020-05-19 23:52:00 -0500

    Solved web-crawler
    Signed-off-by: Emanuel Estrada Larios <emamex98@gmail.com>

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -13,17 +13,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
 
-	"gopl.io/ch5/links"
+	"github.com/todostreaming/gopl.io/ch5/links"
 )
 
 //!+sema
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
+var passed = make(map[string]bool)
+var totalD int
 
 func crawl(url string) []string {
 	fmt.Println(url)
@@ -39,29 +41,68 @@ func crawl(url string) []string {
 
 //!-sema
 
+func preCrawl(url string, depth int, done chan<- bool) {
+
+	defer func() {
+		done <- true
+	}()
+
+	if depth <= 0 {
+		return
+	}
+
+	_, ok := passed[url]
+
+	if ok {
+		return
+	} else {
+		fmt.Print("Depth Level ", (totalD-depth)+1, ": ")
+		passed[url] = true
+	}
+
+	list := crawl(url)
+
+	for _, link := range list {
+		isRecursive := make(chan bool)
+		go preCrawl(link, depth-1, isRecursive)
+		<-isRecursive
+	}
+
+}
+
 //!+
 func main() {
+
+	depth := flag.Int("depth", 1, "depth")
+	flag.Parse()
+
+	totalD = *depth
+
 	worklist := make(chan []string)
-	var n int // number of pending sends to worklist
+	done := make(chan bool)
 
 	// Start with the command-line arguments.
-	n++
-	go func() { worklist <- os.Args[1:] }()
+	go func() { worklist <- flag.Args() }()
+
+	list := <-worklist
+
+	go preCrawl(list[0], *depth, done)
+	<-done
 
 	// Crawl the web concurrently.
-	seen := make(map[string]bool)
-	for ; n > 0; n-- {
-		list := <-worklist
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
-			}
-		}
-	}
+	// seen := make(map[string]bool)
+	// for ; n > 0; n-- {
+	// 	list := <-worklist
+	// 	for _, link := range list {
+	// 		if !seen[link] {
+	// 			seen[link] = true
+	// 			n++
+	// 			go func(link string) {
+	// 				worklist <- crawl(link)
+	// 			}(link)
+	// 		}
+	// 	}
+	// }
 }
 
 //!-
